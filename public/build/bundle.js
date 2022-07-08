22e0210991b813416849b6e66a56d379ca792989
ds797 <73960745+ds797@users.noreply.github.com>
2022-07-08 14:05:55 -0700

    Fix Supabase initialization

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -8453,8 +8453,6 @@ var app = (function () {
     const url = 'https://vjjrqcikqjrfhzgboilx.supabase.co';
     const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqanJxY2lrcWpyZmh6Z2JvaWx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTMwMDUwNzIsImV4cCI6MTk2ODU4MTA3Mn0.Oylk_toXSMsF4sXO4hOr--mj8Wce30Rp7i_uTdlFjKU';
 
-    console.log(createClient(supabaseUrl, supabaseAnonKey));
-
     const supabase = createClient(url, key);
 
     class Timestamp {
