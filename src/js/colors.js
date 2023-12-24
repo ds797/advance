4c05183ad55ab76034fe2d04caa868898c220098
ds797 <73960745+ds797@users.noreply.github.com>
2023-12-24 13:08:56 -0800

    Update colors.js

--- src/js/colors.js
+++ src/js/colors.js
@@ -1,11 +1,26 @@
 export const hexify = (h = 0, s = 100, v = 100) => {
+	h /= 360;
 	s /= 100;
 	v /= 100;
 
-	let l = v * (1 - v / 2);
-	if (l !== 0 && l !== 1) l = (v - l) / Math.min(l, 1 - l);
+	let r, g, b;
 
-	let a = s * Math.min(l, 1 - l);
-  let f = (n, k = (n + h / 30) % 12) => l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-	return `#${Math.floor(f(0) * 255).toString(16).padStart(2, '0')}${Math.floor(f(8) * 255).toString(16).padStart(2, '0')}${Math.floor(f(4) * 255).toString(16).padStart(2, '0')}`;
+  let i = Math.floor(h * 6);
+  let f = h * 6 - i;
+  let p = v * (1 - s);
+  let q = v * (1 - f * s);
+  let t = v * (1 - (1 - f) * s);
+
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+	r = Math.round(r * 255);
+	g = Math.round(g * 255);
+	b = Math.round(b * 255);
+	return `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
 }
